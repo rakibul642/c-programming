e24d2cc3328a94b339b56ebded3c573ca2d2d28b
Rakibul Hasan Shuvo <124507701+rakibul642@users.noreply.github.com>
2023-02-09 22:44:17 +0600

    by shuvo
    shifat program

---
+++ shifat.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+
+printf("\tshifat valo acho vaiya\n");
+printf("\tshifat tmi valo hoye jau\n");
+printf("\tshifat pakhi ke onk valobashe\n");
+printf("\tshifat business korte chay\n");
+
+
+return 0;
+}
---
+++ vai.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+
+printf("\tThis is Rakibul Hasan Shuvo\n");
+printf("\tyou can call me shuvo\n");
+printf("\tI am form Natore\n");
+printf("\tI am a Ex joint secretary Rotaract Club Of Dhaka Urbana\n");
+printf("\tMy Uni Name Diu\n");
+printf("\tI have a big bro of Uni his name is Mohammad Abdullah Al Mamun 31 Batch SWE\n");
+return 0;
+}
